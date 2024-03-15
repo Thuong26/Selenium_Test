ee2a714f324e561ee92d26bcd735396f74d53ab0
Phạm Thị Thương <125046557+Thuong26@users.noreply.github.com>
2024-03-16 01:09:27 +0700

    Add files via upload

---
+++ Selenium_Level1/src/test/java/PageObject/Railway/HomePage.java
@@ -0,0 +1,14 @@
+package PageObject.Railway;
+
+import Common.Constant.Constant;
+
+public class HomePage extends  GeneralPage{
+    //Locators
+    //Elements
+    //Methods
+    public HomePage open(){
+        Constant.WEBDRIVER.navigate().to(Constant.RAILWAY_URL);
+        return this;
+        //This is a method that starts ours test cases
+    }
+}
